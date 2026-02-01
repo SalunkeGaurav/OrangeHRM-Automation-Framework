83dc48c6ef867e36dc49fc6a41066f7208498101
GauravSalunke <gaurav.salunke.work@gmail.com>
2026-02-01 15:57:02 +0530

    Improve BaseTest logging and cleanup

--- src/main/java/com/orangehrm/base/BaseTest.java
+++ src/main/java/com/orangehrm/base/BaseTest.java
@@ -1,4 +1,5 @@
 package com.orangehrm.base;
+
 import com.orangehrm.config.ConfigReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,17 +16,23 @@ public class BaseTest {
     @Parameters("browser")
     @BeforeMethod
     public void setUp(String browser) {
-        log.info("Setting up browser: " + browser);
-        DriverFactory.initDriver(browser);
+        log.info("=== Test Setup Started ===");
+        log.info("Setting up browser: {}", browser);
 
+        DriverFactory.initDriver(browser);
         driver = DriverFactory.getDriver();
-        log.info("Navigating to URL: " + ConfigReader.getProperty("baseUrl"));
-        driver.get(ConfigReader.getProperty("baseUrl"));
+
+        String baseUrl = ConfigReader.getProperty("baseUrl");
+        log.info("Navigating to URL: {}", baseUrl);
+        driver.get(baseUrl);
+
+        log.info("=== Test Setup Completed ===");
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        log.info("Closing browser");
+        log.info("=== Test Teardown Started ===");
         DriverFactory.quitDriver();
+        log.info("=== Test Teardown Completed ===");
     }
 }
